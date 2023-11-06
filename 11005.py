79ecb057dab8ff8a919f6a5c8526e75d80aab81a
hyeon8922 <81852715+hyeon8922@users.noreply.github.com>
2023-11-06 12:09:39 +0900

    [백준] 11005.py

---
+++ 11005.py
@@ -0,0 +1,12 @@
+#11005
+
+import string
+n,b=map(int,input().split())
+tmp = string.digits+string.ascii_lowercase
+def convert(num, base) :
+    q, r = divmod(num, base)
+    if q == 0 :
+        return tmp[r] 
+    else :
+        return convert(q, base) + tmp[r]
+print(convert(n,b).upper())
