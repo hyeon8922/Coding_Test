0b15cf4d917508a5d37956f72bf74600edaa3ac3
hyeon8922 <81852715+hyeon8922@users.noreply.github.com>
2024-03-21 18:56:48 +0900

    [Silver III] Title: 바이러스, Time: 72 ms, Memory: 34028 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,18 +1,18 @@
 from collections import deque
 n = int(input())
-m = int(input())
+v = int(input())
 graph = [[] for _ in range(n+1)]
 visited = [0] * (n+1)
-for i in range(m):
+for i in range(v):
     a, b = map(int, input().split())
     graph[a] += [b]
     graph[b] += [a]
-visited[1] = 1
-Q = deque([1])
-while Q:
-    c = Q.popleft()
-    for nx in graph[c]:
-        if visited[nx] == 0:
-            Q.append(nx)
-            visited[nx] = 1
+
+def dfs(v):
+    visited[v]=1
+    for nx in graph[v]:
+       if visited[nx] == 0:
+           dfs(nx)
+dfs(1) 
+
 print(sum(visited)-1)
