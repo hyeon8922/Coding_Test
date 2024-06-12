8c0f6dc1431659f992ed9978f06dfb2e7b5287bb
hyeon8922 <81852715+hyeon8922@users.noreply.github.com>
2024-06-12 15:51:31 +0900

    Create 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql

---
+++ 프로그래머스/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,6 @@
+SELECT concat('/home/grep/src/',A.BOARD_ID,'/',FILE_ID,FILE_NAME,FILE_EXT) as FILE_PATH
+from USED_GOODS_BOARD A
+join USED_GOODS_FILE B
+on A.BOARD_ID = B.BOARD_ID
+where VIEWS = (select MAX(VIEWS) from USED_GOODS_BOARD)
+order by FILE_ID desc
